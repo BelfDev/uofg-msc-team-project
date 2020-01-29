ed33dd1abf33e04ff1ea0dc298311a0102f1f817
rjs68 <42035943+rjs68@users.noreply.github.com>
2020-01-29 13:09:25 +0000

    Revert "Feature/core/card attribute"

--- src/main/java/com/toptrumps/core/Attribute.java
+++ src/main/java/com/toptrumps/core/Attribute.java
@@ -1,19 +1,4 @@
 package com.toptrumps.core;
 
 public class Attribute {
-    private String attName;
-    private int attValue;
-
-    public Attribute(String attName, int attValue){
-        this.attName = attName;
-        this.attValue = attValue;
-    }
-
-    public String getAttName(){
-        return attName;
-    }
-
-    public String getAttValue(){
-        return attValue;
-    }
 }
--- src/main/java/com/toptrumps/core/Card.java
+++ src/main/java/com/toptrumps/core/Card.java
@@ -1,16 +1,74 @@
 package com.toptrumps.core;
 
-public class Card {
+import com.sun.xml.internal.bind.v2.TODO;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+// TODO: Refactor this class
+public class Card implements Comparable {
     // instance variables
     private String description;
-    private Attribute[] attributes;
+    private HashMap<String, Integer> cardValuesByCat;
+    private ArrayList<String> bestCat;
 
-    public Card(String description, Attribute[] attributes) {
+
+    public Card(String description) {
+
         this.description = description;
-        this.attributes = attributes;
+        cardValuesByCat = new HashMap<>(); // creates new hashmap;
+
+
+    }
+
+    protected void addValueToCat(String cat, int value) { // adds new item to the HashMap
+        cardValuesByCat.put(cat, value);
     }
 
     protected String getDescription() {
         return description;
+    }
+
+    protected ArrayList<String> getBestCat() { return bestCat; }
+
+    protected HashMap<String, Integer> getCardValuesByCat() {
+        return cardValuesByCat;
+    }
+
+    protected int getCat(String cat) {
+        return cardValuesByCat.get(cat);
+    }
+
+    public String toString() {
+        String string = description;
+
+        for (String key : cardValuesByCat.keySet()) {
+            string += " " + key + " " + getCat(key);
+        }
+        return string;
+    }
+
+    protected void findBestCats() {
+        int max = Collections.max(cardValuesByCat.values());
+
+        ArrayList<String> keys = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : cardValuesByCat.entrySet()) {
+            if (entry.getValue() == max) {
+                keys.add(entry.getKey());
+            }
+        }
+        bestCat = keys;
+    }
+
+    //   TODO: Finish this method off.
+    @Override
+    public int compareTo(Object o) {
+        if (o instanceof Card) {
+            return +1;
+        } else {
+            return -1;
+        }
     }
 }
