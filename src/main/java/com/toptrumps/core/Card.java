2b2cf3a7b9358df34e4cefca4817e3476691c55a
Rhys <rhys-stephens@hotmail.co.uk>
2020-01-29 14:44:32 +0000

    Refactored card class to reflect new project structure

--- src/main/java/com/toptrumps/core/Card.java
+++ src/main/java/com/toptrumps/core/Card.java
@@ -1,73 +1,25 @@
 package com.toptrumps.core;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-// TODO: Refactor this class
 public class Card {
     // instance variables
     private String description;
-    private HashMap<String, Integer> cardValuesByCat;
-    private ArrayList<String> bestCat;
+    private Attribute[] attributes;
 
-
-    public Card(String description) {
-
+    /**
+     * Constructor to initiate the description and attributes array
+     * @param String description
+     * @param Attribute[] attributes
+     */
+    public Card(String description, Attribute[] attributes) {
         this.description = description;
-        cardValuesByCat = new HashMap<>(); // creates new hashmap;
-
-
+        this.attributes = attributes;
     }
 
-    protected void addValueToCat(String cat, int value) { // adds new item to the HashMap
-        cardValuesByCat.put(cat, value);
-    }
-
+    /**
+     * Method to retrieve the card description
+     * @return String description
+     */
     public String getDescription() {
         return description;
     }
-
-    public String getBestCat() {
-        if (bestCat.size() == 1) {
-            String best = bestCat.get(0);
-            return best;
-        } else {
-            int index = (int) (Math.random() * bestCat.size());
-            String best = bestCat.get(index);
-            return best;
-        }
-    }
-
-    protected int getCat(String cat) {
-        return cardValuesByCat.get(cat);
-    }
-
-    public String toString() {
-        String string = description;
-
-        for (String key : cardValuesByCat.keySet()) {
-            string += " " + key + " " + getCat(key);
-        }
-        return string;
-    }
-
-    public HashMap<String, Integer> getCardValuesByCat() {
-        return cardValuesByCat;
-    }
-
-    protected void findBestCats() {
-        int max = Collections.max(cardValuesByCat.values());
-
-        ArrayList<String> keys = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : cardValuesByCat.entrySet()) {
-            if (entry.getValue() == max) {
-                keys.add(entry.getKey());
-            }
-        }
-        bestCat = keys;
-    }
-
-
 }
