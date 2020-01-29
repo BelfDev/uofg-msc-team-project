02a114f153892b534fdb165760bc32cdcbe10639
Rhys <rhys-stephens@hotmail.co.uk>
2020-01-29 12:02:04 +0000

    Started structure of attribute class

--- src/main/java/com/toptrumps/core/Attribute.java
+++ src/main/java/com/toptrumps/core/Attribute.java
@@ -1,4 +1,10 @@
 package com.toptrumps.core;
 
 public class Attribute {
+    private String attName;
+    private int attValue;
+
+    public Attribute(String attName, int attValue){
+        
+    }
 }
