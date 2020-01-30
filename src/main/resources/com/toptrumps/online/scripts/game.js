53f87689284ab375781ed00fbbc5293e51a6b560
Anton Samoilov <absamoilov88@gmail.com>
2020-01-30 12:25:00 +0000

    Housekeeping - fixed indentation, move normalize to separate file

--- src/main/resources/com/toptrumps/online/scripts/game.js
+++ src/main/resources/com/toptrumps/online/scripts/game.js
@@ -1,57 +1,57 @@
 const Game = (function() {
-  const init = function() {
-    testAjax();
-    bindEvents();
-  };
+    const init = function() {
+        testAjax();
+        bindEvents();
+    };
 
-  const bindEvents = function() {
-    const $button = $(".js-new-game-button");
-    $($button).on("click", function() {
-      // ...
-      // access button with $(this)
-      $(document).trigger("newGame", { content: "test" });
-    });
-  };
+    const bindEvents = function() {
+        const $button = $(".js-new-game-button");
+        $($button).on("click", function() {
+            // ...
+            // access button with $(this)
+            $(document).trigger("newGame", { content: "test" });
+        });
+    };
 
-  const testAjax = function() {
-    // General AJAX call
-    $.ajax({
-      type: "GET", // or POST,
-      dataType: "text",
-      url: `${restAPIurl}/helloWord`,
-      data: {
-        Word: "Pedro"
-      }
-    })
-      .done(function(data) {
-        console.log(data);
-      })
-      .fail(function(e) {
-        console.error("You have an error: ", e);
-      });
+    const testAjax = function() {
+        // General AJAX call
+        $.ajax({
+            type: "GET", // or POST,
+            dataType: "text",
+            url: `${restAPIurl}/helloWord`,
+            data: {
+                Word: "Pedro"
+            }
+        })
+            .done(function(data) {
+                console.log(data);
+            })
+            .fail(function(e) {
+                console.error("You have an error: ", e);
+            });
 
-    // GET shortcut
-    $.get(`${restAPIurl}/helloWord`, { Word: "Anton" }, function(data) {
-      console.log(data);
-    });
+        // GET shortcut
+        $.get(`${restAPIurl}/helloWord`, { Word: "Anton" }, function(data) {
+            console.log(data);
+        });
 
-    $.getJSON(`${restAPIurl}/helloJSONList`, function(data) {
-      console.log(data);
-    });
+        $.getJSON(`${restAPIurl}/helloJSONList`, function(data) {
+            console.log(data);
+        });
 
-    // POST shortcut
-    $.post(
-      `${restAPIurl}/helloJSONList`,
-      function(data) {
-        console.log(data);
-      },
-      "json"
-    );
-  };
+        // POST shortcut
+        $.post(
+            `${restAPIurl}/helloJSONList`,
+            function(data) {
+                console.log(data);
+            },
+            "json"
+        );
+    };
 
-  return {
-    init
-  };
+    return {
+        init
+    };
 })();
 
 $(document).ready(Game.init);
