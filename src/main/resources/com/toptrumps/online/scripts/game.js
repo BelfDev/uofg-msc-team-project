de3f3a3a9d940517ff2ffd6143b5418dae02706a
Anton Samoilov <absamoilov88@gmail.com>
2020-01-31 18:44:48 +0000

    Remove test js code

--- src/main/resources/com/toptrumps/online/scripts/game.js
+++ src/main/resources/com/toptrumps/online/scripts/game.js
@@ -1,53 +1,5 @@
 const Game = (function() {
-    const init = function() {
-        testAjax();
-        bindEvents();
-    };
-
-    const bindEvents = function() {
-        const $button = $(".js-new-game-button");
-        $($button).on("click", function() {
-            // ...
-            // access button with $(this)
-            $(document).trigger("newGame", { content: "test" });
-        });
-    };
-
-    const testAjax = function() {
-        // General AJAX call
-        $.ajax({
-            type: "GET", // or POST,
-            dataType: "text",
-            url: `${restAPIurl}/helloWord`,
-            data: {
-                Word: "Pedro"
-            }
-        })
-            .done(function(data) {
-                console.log(data);
-            })
-            .fail(function(e) {
-                console.error("You have an error: ", e);
-            });
-
-        // GET shortcut
-        $.get(`${restAPIurl}/helloWord`, { Word: "Anton" }, function(data) {
-            console.log(data);
-        });
-
-        $.getJSON(`${restAPIurl}/helloJSONList`, function(data) {
-            console.log(data);
-        });
-
-        // POST shortcut
-        $.post(
-            `${restAPIurl}/helloJSONList`,
-            function(data) {
-                console.log(data);
-            },
-            "json"
-        );
-    };
+    const init = function() {};
 
     return {
         init
