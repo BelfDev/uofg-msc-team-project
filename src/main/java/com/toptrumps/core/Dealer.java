27255e99ee3a64734478d5305d8d3be533cb0be6
Rhys <rhys-stephens@hotmail.co.uk>
2020-02-02 11:27:06 +0000

    Moved DeckParser to constructor of Dealer, saved deck in the instance variable within dealer

--- src/main/java/com/toptrumps/core/Dealer.java
+++ src/main/java/com/toptrumps/core/Dealer.java
@@ -1,4 +1,10 @@
 package com.toptrumps.core;
 
 public class Dealer {
+    Deck deck;
+
+    public Dealer(ArrayList<Player){
+        DeckParser d = new DeckParser();
+        deck = d.getCards();
+    }
 }
