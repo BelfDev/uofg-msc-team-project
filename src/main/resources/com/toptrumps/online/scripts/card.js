190f748d3b9a3a72bbcb3d0f8a94a3cb9751f7bf
Anton Samoilov <absamoilov88@gmail.com>
2020-02-02 00:14:57 +0000

    Add js to show top card after AJAX call

--- src/main/resources/com/toptrumps/online/scripts/card.js
+++ src/main/resources/com/toptrumps/online/scripts/card.js
@@ -1,1 +1,56 @@
-const Card = (function() {})();
+const Card = (function() {
+    const cardAttributeSelector = ".js-card-char";
+    const cardAttributeValueSelector = ".js-card-char-value";
+    const cardAttributesWrapperSelector = ".js-card-chars";
+    const cardImageSelector = ".js-card-image";
+    const cardTitleSelector = ".js-card-title";
+
+    const getImagePath = function(cardName) {
+        const imageName = cardName.replace(" ", "_").toLowerCase();
+        return `/assets/images/${imageName}.png`;
+    };
+
+    const getAttributes = function(playerSelector, attributesData) {
+        let attrNodeCollection = [];
+        const attrTpl = $(playerSelector).find(cardAttributeSelector);
+
+        $.each(attributesData, function(name, value) {
+            const attrNode = $(attrTpl).clone();
+            attrNode.prepend(name);
+            attrNode.find(cardAttributeValueSelector).html(value);
+            attrNodeCollection.push(attrNode);
+        });
+
+        return attrNodeCollection;
+    };
+
+    const update = function(playerID, data) {
+        let playerSelector = Player.getPlayerSelector(playerID);
+
+        // Set image src and title
+        $(playerSelector)
+            .find(cardImageSelector)
+            .attr("src", getImagePath(data.name))
+            .attr("title", data.name);
+
+        // Set card title
+        $(playerSelector)
+            .find(cardTitleSelector)
+            .html(data.name);
+
+        // Get attributes html
+        const attrNodeCollection = getAttributes(
+            playerSelector,
+            data.attributes
+        );
+
+        // Set attributes
+        $(playerSelector)
+            .find(cardAttributesWrapperSelector)
+            .append(attrNodeCollection);
+    };
+
+    return {
+        update
+    };
+})();
--- src/main/resources/com/toptrumps/online/views/_parts/footer.ftl
+++ src/main/resources/com/toptrumps/online/views/_parts/footer.ftl
@@ -1,5 +1,8 @@
 	<!-- Import JQuery, as it provides functions you will probably find useful (see https://jquery.com/) -->
     <script src="/assets/scripts/jquery-3.4.1.min.js"></script>
+	<script src="/assets/scripts/message.js"></script>
+	<script src="/assets/scripts/card.js"></script>
+	<script src="/assets/scripts/player.js"></script>
 	<script src="/assets/scripts/game.js"></script>
 	<script>
 		const restAPIurl = 'http://localhost:7777/toptrumps';
