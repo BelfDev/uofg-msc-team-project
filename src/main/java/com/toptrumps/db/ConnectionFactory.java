23c4678f52df4c8fcdc111494f9c40d7d08ce71e
nathanieldschneider <nathanieldanielschneider@gmail.com>
2020-02-02 13:52:41 +0000

    Added database connection functionality

---
+++ src/main/java/com/toptrumps/db/ConnectionFactory.java
@@ -0,0 +1,32 @@
+package com.toptrumps.db;
+    import java.sql.*; 
+    import java.sql.DriverManager; 
+    import java.sql.Connection; 
+    import java.sql.ResultSet; 
+    import java.sql.SQLException; 
+    import java.sql.Statement; 
+    
+    public class ConnectionFactory { 
+
+        private static String localhost = "jdbc:postgresql://localhost:5432/";
+        private static String username = "postgres";
+        private static String password = "postgres";
+
+        public static Connection getConnection() { 
+          
+            Connection connection = null; 
+            try { 
+                connection = DriverManager.getConnection(localhost,username,password); 
+            } catch (SQLException e) { 
+                System.out.println("Connection Failed!"); 
+                e.printStackTrace(); 
+            } 
+            if (connection != null) { 
+                System.out.println("You are in control."); 
+            } 
+            else { 
+                System.out.println("Failed to establish connection!"); 
+            } 
+            return connection;
+        } 
+    } 
