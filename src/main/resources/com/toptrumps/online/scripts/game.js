34960582c0115508ac1afc3d7ff8d5df39ae0600
Anton Samoilov <absamoilov88@gmail.com>
2020-02-03 14:25:48 +0000

    Add method to get player id

--- src/main/resources/com/toptrumps/online/scripts/game.js
+++ src/main/resources/com/toptrumps/online/scripts/game.js
@@ -1,6 +1,15 @@
 const Game = (function() {
     const init = function() {
         Player.getTopCard(0);
+        getActivePlayer();
+    };
+
+    const getActivePlayer = function() {
+        $.get(`${restAPIurl}/getActivePlayer`, function(response) {
+            // TODO: use player id to check if human player can choose category
+            // or send another request to get AI player's choice
+            console.log(response.playerID);
+        });
     };
 
     return {
