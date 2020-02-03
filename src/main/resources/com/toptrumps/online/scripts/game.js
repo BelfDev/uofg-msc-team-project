4026a3db584b4aa6a8789c3841a46761fafda4d7
Anton Samoilov <absamoilov88@gmail.com>
2020-02-03 19:44:33 +0000

    Add category selection if a human player is active

--- src/main/resources/com/toptrumps/online/scripts/game.js
+++ src/main/resources/com/toptrumps/online/scripts/game.js
@@ -1,19 +1,77 @@
 const Game = (function() {
+    const NUMBER_OF_AI_PLAYERS = 4;
+    const endTurnButtonSelector = ".js-end-turn-button";
+
+    let activeCategory = null;
+
     const init = function() {
+        runStartPhase();
+        bindEvents();
+    };
+
+    const bindEvents = function() {
+        $(document).on("game:setActivePlayer", function(event, data) {
+            runCategorySelectionPhase(data.playerID);
+        });
+
+        $(document).on("game:categorySelect", function(event, data) {
+            activeCategory = data.category;
+        });
+
+        $(endTurnButtonSelector).on("click", function(e) {
+            if (activeCategory !== null) {
+                runRoundConclusionPhase();
+            }
+
+            e.preventDefault();
+        });
+    };
+
+    const runStartPhase = function() {
         Player.getTopCard(0);
         getActivePlayer();
     };
 
+    const runCategorySelectionPhase = function(playerID) {
+        // Human player has id 0
+        if (playerID === 0) {
+            $(document).trigger("message:log", {
+                content: "You are an active player. Choose a category"
+            });
+
+            Card.enableCategorySelection();
+        } else {
+            Card.disableCategorySelection();
+            // TODO: send request to get chosen category
+        }
+    };
+
+    const runRoundConclusionPhase = function() {
+        console.log("runRoundConclusionPhase");
+        // TODO: reveal other players cards and announce the winner
+    };
+
     const getActivePlayer = function() {
         $.get(`${restAPIurl}/getActivePlayer`, function(response) {
-            // TODO: use player id to check if human player can choose category
-            // or send another request to get AI player's choice
-            console.log(response.playerID);
+            if (
+                response.playerID < 0 ||
+                response.playerID > NUMBER_OF_AI_PLAYERS
+            ) {
+                throw new Error(
+                    `Player ID must be between 0 and ${NUMBER_OF_AI_PLAYERS}`
+                );
+                return false;
+            }
+
+            $(document).trigger("game:setActivePlayer", {
+                playerID: response.playerID
+            });
         });
     };
 
     return {
-        init
+        init,
+        NUMBER_OF_AI_PLAYERS
     };
 })();
 
