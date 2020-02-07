21dfe5941259cd67a9668df7e48bc139db297411
Rhys <rhys-stephens@hotmail.co.uk>
2020-02-07 21:31:22 +0000

    Added animation and pauses to the displayed text

--- src/main/java/com/toptrumps/cli/CommandLineView.java
+++ src/main/java/com/toptrumps/cli/CommandLineView.java
@@ -5,6 +5,8 @@ import com.toptrumps.core.card.Card;
 import com.toptrumps.core.player.Player;
 
 import java.util.List;
+import java.lang.Thread;
+import java.lang.InterruptedException;
 
 public class CommandLineView {
 
@@ -13,16 +15,18 @@ public class CommandLineView {
     }
 
     public void showRoundStart(Player activePlayer, Player humanPlayer, int roundNumber){
+        pausePrinting(1000);
         showRoundNumber(roundNumber);
         showActivePlayer(activePlayer, humanPlayer);
         if(!humanPlayer.isAIPlayer()){
+            pausePrinting(500);
             showNumberOfHumanCards(humanPlayer);
             showCard(humanPlayer.getTopCard());
         }
     }
 
     private void showRoundNumber(int roundNumber){
-        System.out.println("--------------------");
+        System.out.println("\n\n--------------------");
         System.out.println("    Round " + roundNumber);
         System.out.println("--------------------");
     }
@@ -34,37 +38,52 @@ public class CommandLineView {
         }else {
             message += activePlayer.getName() + " is the active player";
         }
-        System.out.println(message);
+        typePrint(20, message);
     }
 
     private void showNumberOfHumanCards(Player humanPlayer){
-        int numberOfHumanCards = humanPlayer.getDeckCount();
-        String cardOrCards = "cards";
-        if(numberOfHumanCards == 1){
-            cardOrCards = "card";
+        if(!humanPlayer.isAIPlayer()){
+            int numberOfHumanCards = humanPlayer.getDeckCount();
+            String cardOrCards = numberOfHumanCards==1 ? "card" : "cards";
+            String message = String.format("You have %d %s in your hand", numberOfHumanCards, cardOrCards);
+            typePrint(20, message);
         }
-        String message = String.format("You have %d %s in your hand", numberOfHumanCards, cardOrCards);
-        System.out.println(message);
     }
 
     private void showCard(Card card){
         List<Attribute> attributes = card.getAttributes();
         String name = card.getName();
+        pausePrinting(1000);
 
-        System.out.println("Your card is:");
-        System.out.println("\t--------------------");
-        System.out.println(String.format("\t|%18s|", name));
-        System.out.println(String.format("\t|%18s|", ""));
-        System.out.println(String.format("\t|%18s|", ""));
+        typePrint(20, "\nYour card is:");
+        typePrint(5, "\t--------------------");
+        typePrint(5, String.format("\t|%18s|", name));
+        typePrint(5, String.format("\t|%18s|", ""));
+        typePrint(5, String.format("\t|%18s|", ""));
 
         for (int i = 0; i < attributes.size(); i++) {
             String attributeName = attributes.get(i).getName();
             int attributeValue = attributes.get(i).getValue();
             String message = String.format("\t|%d: %-13s%2d|", i + 1, attributeName, attributeValue);
-            System.out.println(message);
+            typePrint(5, message);
         }
 
-        System.out.println("\t--------------------");
+        typePrint(5, "\t--------------------");
     }
 
+    private void pausePrinting(int pauseTime){
+        try {
+            Thread.sleep(pauseTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void typePrint(int characterPause, String printLine){
+        for(String s: printLine.split("")){
+            System.out.print(s);
+            pausePrinting(characterPause);
+        }
+        System.out.print("\n");
+    }
 }
