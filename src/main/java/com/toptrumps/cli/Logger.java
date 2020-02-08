97bc5a274154ef276ad73ddf908e1b3ce3992d11
hughwin <hugh.winchester@gmail.com>
2020-02-08 13:44:36 +0000

    reworked logger to remove conflicts

--- src/main/java/com/toptrumps/cli/Logger.java
+++ src/main/java/com/toptrumps/cli/Logger.java
@@ -1,4 +1,45 @@
 package com.toptrumps.cli;
 
+import java.io.IOException;
+import java.util.logging.*;
+
 public class Logger {
+    public static final String LOGGER = "commandLineLogger";
+    public static final String DIVIDER = "-------------------";
+
+    private final String filePath;
+    java.util.logging.Logger logger;
+    private Handler loggerHandler;
+
+
+
+    public Logger(String filePath){
+        logger = java.util.logging.Logger.getLogger(LOGGER);
+
+        this.filePath = filePath;
+    }
+
+    public void enable(){
+        try {
+            loggerHandler = new FileHandler(filePath);
+
+            loggerHandler.setFormatter(new Formatter() {
+                @Override
+                public String format(LogRecord record) {
+                    return String.format(("%s\n%s\n"), record.getMessage(), DIVIDER);
+                }
+            });
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        logger.addHandler(loggerHandler);
+        logger.setLevel(java.util.logging.Level.INFO);
+    }
+
+    public static void logToFile(String logEntry){
+        java.util.logging.Logger.getLogger(LOGGER).info(logEntry);
+    }
+
 }
--- src/main/java/com/toptrumps/cli/TopTrumpsCLIApp.java
+++ src/main/java/com/toptrumps/cli/TopTrumpsCLIApp.java
@@ -1,10 +1,18 @@
 package com.toptrumps.cli;
 
 
+import ch.qos.logback.core.util.FileUtil;
+
+import java.io.File;
+
 /**
  * Top Trumps command line application
  */
 public class TopTrumpsCLIApp {
+
+    private static final String LOG_PATH = "./";
+    private static final String LOG_FILENAME = "TopTrumps.log";
+
 
     /**
      * This main method is called by TopTrumps.java when the user specifies that they want to run in
@@ -12,12 +20,19 @@ public class TopTrumpsCLIApp {
      */
     public static void main(String[] args) {
 
+
         boolean writeGameLogsToFile = false; // Should we write game logs to file?
-         if (args[0].equalsIgnoreCase("true")) writeGameLogsToFile = true; // Command line selection
+        if (args[0].equalsIgnoreCase("true")) writeGameLogsToFile = true; // Command line selection
+
+
+        if (writeGameLogsToFile) {
+            Logger logger = new Logger(LOG_PATH + LOG_FILENAME);
+            logger.enable();
+        }
+        Logger.logToFile("MAD STAX FTW.");
+        // TODO remove this.
 
         CommandLineController commandLineUI = new CommandLineController();
         commandLineUI.start();
-
     }
-
 }
