45ba142600cc1cdb77e629b19557c250efc96d10
Pedro Belfort <belfortdev@gmail.com>
2020-02-12 20:18:52 +0000

    Prevent code users to instantiate a RandomGenerator

--- src/main/java/com/toptrumps/core/utils/RandomGenerator.java
+++ src/main/java/com/toptrumps/core/utils/RandomGenerator.java
@@ -4,6 +4,9 @@ import java.util.Random;
 
 public class RandomGenerator {
 
+    private RandomGenerator() {
+    }
+
     public static int getInteger(int min, int max) {
         Random randomGenerator = new Random();
         return randomGenerator.nextInt((max - min) + 1) + min;
