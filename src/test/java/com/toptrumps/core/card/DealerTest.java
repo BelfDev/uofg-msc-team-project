724801f61b4d1fdec36c4ecf8cfcc0d15d8ddea5
Pedro Belfort <belfortdev@gmail.com>
2020-02-13 14:54:48 +0000

    Add core game engine unit tests

--- src/test/java/com/toptrumps/core/card/DealerTest.java
+++ src/test/java/com/toptrumps/core/card/DealerTest.java
@@ -10,25 +10,26 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
 @TestInstance(PER_CLASS)
-public class DealerTest {
+class DealerTest {
 
     private final static String DECK_RESOURCE = "assets/WitcherDeck.txt";
+
     private Dealer dealer;
 
     @BeforeAll
-    public void oneTimeSetUp() {
+    void oneTimeSetUp() {
         this.dealer = new Dealer(DECK_RESOURCE);
     }
 
     @Test
     @DisplayName("Dealer initialization populates deck")
-    public void newDealer_DeckResourcePath_ShouldPopulateCardList() {
+    void newDealer_DeckResourcePath_ShouldPopulateCardList() {
         assertFalse(dealer.getDeck().isEmpty());
     }
 
     @Test
     @DisplayName("Dealing cards shuffles the deck")
-    public void dealCards_NumberOfPlayers_ShuffledDeck() {
+    void dealCards_NumberOfPlayers_ShuffledDeck() {
         List<Card> originalDeck = new ArrayList<>(dealer.getDeck());
         dealer.dealCards(4);
         ArrayList<Card> shuffledDeck = dealer.getDeck();
@@ -37,10 +38,10 @@ public class DealerTest {
 
     @Test
     @DisplayName("Dealing cards splits the deck between the number of players")
-    public void dealCards_NumberOfPlayers_SplitDecks() {
+    void dealCards_NumberOfPlayers_SplitDecks() {
         int numberOfPlayers = 4;
         ArrayList<ArrayList<Card>> decks = dealer.dealCards(numberOfPlayers);
-        assertEquals(decks.size(), numberOfPlayers);
+        assertEquals(numberOfPlayers, decks.size());
     }
 
 }
